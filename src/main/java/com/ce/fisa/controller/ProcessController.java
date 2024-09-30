624bdff6dc9a7f65f19dc14ed292dc55b3047238
Yeeun Heo <38968449+yyyeun@users.noreply.github.com>
2024-09-30 16:33:20 +0900

    Update ProcessController.java

--- src/main/java/com/ce/fisa/controller/ProcessController.java
+++ src/main/java/com/ce/fisa/controller/ProcessController.java
@@ -12,6 +12,6 @@ public class ProcessController {
 	@GetMapping("/test")
 	public String reqRes() {
 		log.info("요청 수락 ~~~");
-		return "linux 서버에서 실행되는 app + cicdbasic.sh 적용";
+		return "linux 서버에서 실행되는 app + inotify-tools 적용";
 	}
 }
