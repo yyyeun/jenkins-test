0066014933bd401a673e73f274c17e71790fa50c
Yeeun Heo <38968449+yyyeun@users.noreply.github.com>
2024-10-01 09:45:24 +0900

    Update ProcessController.java

--- src/main/java/com/ce/fisa/controller/ProcessController.java
+++ src/main/java/com/ce/fisa/controller/ProcessController.java
@@ -12,6 +12,6 @@ public class ProcessController {
 	@GetMapping("/test")
 	public String reqRes() {
 		log.info("요청 수락 ~~~");
-		return "linux 서버에서 실행되는 app + 1001";
+		return "linux 서버에서 실행되는 app + 1001 test";
 	}
 }
